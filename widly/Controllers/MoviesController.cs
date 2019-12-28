e125b0ddb84b12370fc259554ad6192a003eade2
chern007 <carloshernandezcrespo@gmail.com>
2019-12-28 19:06:49 +0100

    Agregar archivos de proyecto.

---
+++ widly/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using widly.Models;
+
+namespace widly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+
+            var movie = new Movies() { Name = "Shrek!!!" };
+
+
+            //return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+    }
+}
